2ab2902a835bc294827b0d3cd6927d07e53e28f2
Sebastian Lohff <sebastian.lohff@sap.com>
2019-04-23 11:14:29 +0200

    Add setup.py to make this project installable

---
+++ setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='docker-image-patcher',
+    version='0.1.0',
+    description='',
+    author='Sebastian Lohff',
+    author_email='sebastian.lohff@sap.com',
+    url='https://github.wdf.sap.corp/tg17/docker-image-patcher',
+    python_requires='>=3.5',
+    packages=['docker_image_patcher'],
+    install_requires=['fs', 'docker'],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Environment :: Console',
+    ],
+    entry_points={
+        'console_scripts': [
+            'docker-image-patch = docker_image_patcher.docker_image_patch:main'
+        ]
+    },
+)
